8e31fcfa2ba810e73ed7766f12fe5b26fc80cc1c
NULL-header <poaoq.poaoq@gmail.com>
2020-12-12 01:23:07 +0900

    update: adjust type #5

--- src/createTheme.tsx
+++ src/createTheme.tsx
@@ -1,58 +1,133 @@
-import React, { createContext, useContext, useReducer } from "react";
+import React, { createContext, useContext, Reducer, useCallback } from "react";
+import { useReducerAsync, AsyncActionHandlers } from "use-reducer-async";
 import { createTheming } from "react-jss";
+import { KVSIndexedDB } from "@kvs/indexeddb";
 
 import { createMakeStyles } from "./createMakeStyles";
-// eslint-disable-next-line no-unused-vars
-import { CreateThemeTypes } from "./types";
+
+type CreateThemeTypes<ThemeNames> = {
+  Reduce: {
+    State: { isLoading: boolean; themeName: ThemeNames; isSetting: boolean };
+    Action:
+      | { type: "FINISH_LOADING" }
+      | { type: "CHANGE_THEMENAME"; themeName: ThemeNames }
+      | { type: "START_SETTING_TO_DB" };
+    AsyncAction:
+      | {
+          type: "SET_THEME_TO_DB";
+          db: KVSIndexedDB<any>;
+          cacheKey: string;
+          themeName: ThemeNames;
+        }
+      | {
+          type: "LOAD_THEME_FROM_DB";
+          db: KVSIndexedDB<any>;
+          cacheKey: string;
+          defaultThemeName: ThemeNames;
+        };
+    Reducer: Reducer<
+      CreateThemeTypes<ThemeNames>["Reduce"]["State"],
+      CreateThemeTypes<ThemeNames>["Reduce"]["Action"]
+    >;
+    AsyncReducer: AsyncActionHandlers<
+      CreateThemeTypes<ThemeNames>["Reduce"]["Reducer"],
+      CreateThemeTypes<ThemeNames>["Reduce"]["AsyncAction"]
+    >;
+  };
+  ThemeNameContent: [ThemeNames, (themeName: ThemeNames) => void];
+};
 
 const createReducer = function <
   Reduce extends CreateThemeTypes<any>["Reduce"]
 >() {
   const reducer: Reduce["Reducer"] = (state, action) => {
     switch (action.type) {
-      case "finishLoading": {
+      case "FINISH_LOADING": {
         return { ...state, isLoading: false };
       }
-      case "changeTheme": {
-        return { ...state, themeName: action.themeName };
+      case "CHANGE_THEMENAME": {
+        return { ...state, themeName: action.themeName, isSetting: false };
+      }
+      case "START_SETTING_TO_DB": {
+        return { ...state, isSetting: true };
+      }
+      default: {
+        throw new Error("unknown case at the reducer");
       }
     }
   };
-  return reducer;
+  const asyncReducer: Reduce["AsyncReducer"] = {
+    SET_THEME_TO_DB: ({ dispatch, signal }) => async (action) => {
+      dispatch({ type: "START_SETTING_TO_DB" });
+      await action.db.set(action.cacheKey, action.themeName);
+      if (!signal.aborted)
+        dispatch({ type: "CHANGE_THEMENAME", themeName: action.themeName });
+    },
+    LOAD_THEME_FROM_DB: ({ dispatch, signal }) => async (action) => {
+      const cache = await action.db.get(action.cacheKey);
+      const themeName = cache == null ? action.defaultThemeName : cache;
+      if (!signal.aborted) dispatch({ type: "CHANGE_THEMENAME", themeName });
+    },
+  };
+  return { reducer, asyncReducer };
 };
 
-export const createSwithableTheme = function <Theme, ThemeNames extends string>(
+export const createTheme = function <Theme, ThemeNames extends string>(
   themes: Record<ThemeNames, Theme>
 ) {
   type Types = CreateThemeTypes<ThemeNames>;
+
   const ThemeContext = createContext({} as Theme);
   const Theming = createTheming(ThemeContext);
-  const reducer = createReducer<Types["Reduce"]>();
-  const ThemeNameContext = createContext({} as Types["ThemeNameContent"]);
 
   const makeStyles = createMakeStyles(Theming);
 
+  const ThemeNameContext = createContext({} as Types["ThemeNameContent"]);
   const useThemeName = () => useContext(ThemeNameContext);
 
+  const { asyncReducer, reducer } = createReducer<Types["Reduce"]>();
   const firstThemeName = Object.keys(themes)[0] as ThemeNames;
   const defaultState: Types["Reduce"]["State"] = {
-    isLoading: false,
+    isLoading: true,
     themeName: firstThemeName,
+    isSetting: false,
   };
-  const ThemeProvider: React.FC = (props) => {
-    const [state, dispatch] = useReducer(reducer, defaultState);
-    const themeNameValue: Types["ThemeNameContent"] = [
-      state.themeName,
-      (themeName: ThemeNames) => dispatch({ type: "changeTheme", themeName }),
-    ];
-    const theme = themes[state.themeName];
-    return (
-      <Theming.ThemeProvider theme={theme as any}>
-        <ThemeNameContext.Provider value={themeNameValue}>
-          {props.children}
-        </ThemeNameContext.Provider>
-      </Theming.ThemeProvider>
+  const ThemeProvider: React.FC<{
+    db: KVSIndexedDB<any>;
+    cacheKey: string;
+    LodingComponent: () => JSX.Element;
+  }> = React.memo((props) => {
+    const [state, dispatch] = useReducerAsync(
+      reducer,
+      defaultState,
+      asyncReducer
     );
-  };
-  return { useThemeName, ThemeProvider, makeStyles };
+    const themeSetter = useCallback(
+      (themeName: ThemeNames) =>
+        dispatch({
+          type: "SET_THEME_TO_DB",
+          cacheKey: props.cacheKey,
+          db: props.db,
+          themeName,
+        }),
+      [dispatch, props.cacheKey, props.db]
+    );
+    if (state.isLoading) {
+      return <props.LodingComponent />;
+    } else {
+      const themeNameValue: Types["ThemeNameContent"] = [
+        state.themeName,
+        themeSetter,
+      ];
+      const theme = themes[state.themeName];
+      return (
+        <Theming.ThemeProvider theme={theme as any}>
+          <ThemeNameContext.Provider value={themeNameValue}>
+            {props.children}
+          </ThemeNameContext.Provider>
+        </Theming.ThemeProvider>
+      );
+    }
+  });
+  return { useThemeName, ThemeProvider, makeStyles, Theming };
 };
