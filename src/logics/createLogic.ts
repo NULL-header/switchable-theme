bd34a3985d597fa986b2940a2859484c8a3e51cf
NULL-header <poaoq.poaoq@gmail.com>
2021-01-02 18:26:53 +0000

    remove: unused files #12

--- src/logics/createLogic.ts
+++
@@ -1,8 +0,0 @@
-import { UseThemeArgs } from "../useTheme";
-
-export type Logic<T, Args extends unknown[]> = <ThemeNames extends string>(
-  useThemeArg: UseThemeArgs<ThemeNames>
-) => (signalArg: AbortController, ...args: Args) => T;
-
-export const createLogic = <T, U extends unknown[]>(logic: Logic<T, U>) =>
-  logic;
